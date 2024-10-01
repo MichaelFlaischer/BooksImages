6dd96d7b810bded285bfb752792a919c99efa374
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-10-01 15:11:32 +0300

    Added support for a dynamic component that allows users to choose their preferred method for rating a book.

--- cmps/AddReview.jsx
+++ cmps/AddReview.jsx
@@ -4,6 +4,37 @@ const { useParams, useNavigate } = ReactRouterDOM
 import { bookService } from '../services/book.service.js'
 import { eventBusService } from '../services/event-bus.service.js'
 
+function RateByStars({ rating, onSelect }) {
+  return (
+    <div className='star-rating'>
+      {[5, 4, 3, 2, 1].map((num) => (
+        <React.Fragment key={num}>
+          <input type='radio' id={`star${num}`} name='rating' value={num} checked={rating === num} onChange={() => onSelect(num)} />
+          <label htmlFor={`star${num}`} title={`${num} stars`}>
+            ★
+          </label>
+        </React.Fragment>
+      ))}
+    </div>
+  )
+}
+
+function RateBySelect({ rating, onSelect }) {
+  return (
+    <select value={rating} onChange={(ev) => onSelect(+ev.target.value)}>
+      {[1, 2, 3, 4, 5].map((num) => (
+        <option key={num} value={num}>
+          {num} Stars
+        </option>
+      ))}
+    </select>
+  )
+}
+
+function RateByTextbox({ rating, onSelect }) {
+  return <input type='number' min='1' max='5' value={rating} onChange={(ev) => onSelect(+ev.target.value)} />
+}
+
 export function AddReview() {
   const { bookId } = useParams()
   const navigate = useNavigate()
@@ -16,6 +47,7 @@ export function AddReview() {
   const [bookTitle, setBookTitle] = useState('')
   const [isSaveDisabled, setIsSaveDisabled] = useState(true)
   const [selectedRating, setSelectedRating] = useState(review.rating)
+  const [ratingType, setRatingType] = useState('stars')
 
   useEffect(() => {
     bookService
@@ -51,7 +83,7 @@ export function AddReview() {
     }))
   }
 
-  function handleStarClick(rating) {
+  function handleRatingSelect(rating) {
     setSelectedRating(rating)
     setReview((prevReview) => ({
       ...prevReview,
@@ -96,19 +128,33 @@ export function AddReview() {
             </tr>
             <tr>
               <td>
+                <label>Rating Type:</label>
+              </td>
+              <td>
+                <div>
+                  <label>
+                    <input type='radio' name='ratingType' value='stars' checked={ratingType === 'stars'} onChange={() => setRatingType('stars')} />
+                    Stars
+                  </label>
+                  <label>
+                    <input type='radio' name='ratingType' value='select' checked={ratingType === 'select'} onChange={() => setRatingType('select')} />
+                    Select
+                  </label>
+                  <label>
+                    <input type='radio' name='ratingType' value='textbox' checked={ratingType === 'textbox'} onChange={() => setRatingType('textbox')} />
+                    Textbox
+                  </label>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
                 <label>Rating:</label>
               </td>
               <td>
-                <div className='star-rating'>
-                  {[5, 4, 3, 2, 1].map((num) => (
-                    <React.Fragment key={num}>
-                      <input type='radio' id={`star${num}`} name='rating' value={num} checked={selectedRating === num} onChange={() => handleStarClick(num)} />
-                      <label htmlFor={`star${num}`} title={`${num} stars`}>
-                        ★
-                      </label>
-                    </React.Fragment>
-                  ))}
-                </div>
+                {ratingType === 'stars' && <RateByStars rating={selectedRating} onSelect={handleRatingSelect} />}
+                {ratingType === 'select' && <RateBySelect rating={selectedRating} onSelect={handleRatingSelect} />}
+                {ratingType === 'textbox' && <RateByTextbox rating={selectedRating} onSelect={handleRatingSelect} />}
               </td>
             </tr>
             <tr>
